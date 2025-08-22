ccb13f411f96bdf9488821b92233b5bfa172463f
Camilo Vargas <ca06vargas@gmail.com>
2025-08-21 23:01:30 -0500

    Add initial client setup with Vite, React, and TypeScript
    - Create a new client directory structure with essential files including index.html, App component, and CSS styles.
    - Introduce configuration files for TypeScript and ESLint to ensure code quality and maintainability.
    - Set up package.json with dependencies for React, Vite, and TypeScript, along with scripts for development and build processes.
    - Add .gitignore to exclude unnecessary files and directories from version control.
    - Implement basic styling and structure for the application, enhancing the initial user interface.

---
+++ client/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "../web",
+    cssMinify: true,
+    minify: "esbuild",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
